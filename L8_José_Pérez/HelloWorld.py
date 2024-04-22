9e226741f3bd35f28308c19236cc007fd0e14573
ElIngePerez <jrperezch@correo.url.edu.gt>
2024-04-22 08:30:52 -0600

    Laboratorios 8-12

---
+++ L8_José_Pérez/HelloWorld.py
@@ -0,0 +1,7 @@
+Nombre = input ("Ingrese su nombre \n")
+print("Hola", Nombre )
+
+Numero1 = int(input("Ingrese un número \n"))
+Numero2 = int(input("Ingrese un número \n"))
+
+print("Suma", Numero1 + Numero2)
